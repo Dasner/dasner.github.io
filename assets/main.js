9dbfd8b7c00bae52371eba48276c8a8879bf6bc8
Cox, Dana <Dana_Cox@jhancock.com>
2018-01-23 13:05:16 -0500

    added javascript

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+// main.js
+
+const $navMenu = document.getElementById('navMenu');
+const $closeMenuButton = document.getElementById('closeMenuButton');
+const $openMenuButton = document.getElementById('openMenuButton');
+
+function toggleMenu() {
+	// if the menu is hidden
+	if ($navMenu.classList.contains('mobile-hidden')) {
+		//make it visible
+		$navMenu.classList.remove('mobile-hidden');
+	} else {//otherwise
+		//make it hidden
+		$navMenu.classList.add('mobile-hidden');
+	}
+		
+			
+}
+
+// when I click the "X" the menu should hide.
+$closeMenuButton.addEventListener('click', toggleMenu);
+
+//when I click the hamburger menu button, the menu should open.
+$openMenuButton.addEventListener('click', toggleMenu);
